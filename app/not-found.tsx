8bd837ac12a45538d357d745a8def45d1360087b
omarHussamm <83400756+omarHussamm@users.noreply.github.com>
2025-03-08 22:48:37 +0200

    added user models

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -10,7 +10,7 @@ const NotFoundPage = () => {
         <div className="p-6 w-1/3 rounded-lg shadow-md text-center">
         <h1 className="text-3xl font-bold mb-4">Not Fond</h1>
         <p className="text-destructive">Could not find requested page</p>
-        <Button variant={'outline'} className="mt-4 ml-2" asChild><Link href="/" title="Back To Home" /></Button>
+        <Button variant={'outline'} className="mt-4 ml-2" asChild><Link href="/">Back To Home</Link></Button>
         </div>
     </div>;
 }
